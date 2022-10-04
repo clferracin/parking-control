b172f4981069c65e83663039463f9c01178f50d8
Cesar Ferracin <cesarferracin@gmail.com>
2022-10-03 22:35:10 -0300

    parking control spring boot project

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,22 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+	
+	boolean existsByLicensePlateCar(String licensePlateCar);
+	
+	boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	
+	boolean existsByApartmentAndBlock(String apartment, String block);
+	
+	List<ParkingSpotModel> findAll();
+
+}
